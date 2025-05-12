55466ac23bfe8d6ece24f3eb1245d9fdbd2eaea5
yoonsunho <y_sunho@naver.com>
2025-05-12 11:17:58 +0900

    vue03

---
+++ vue/04-single-file-component/MyComponent.vue
@@ -0,0 +1,17 @@
+<!-- MyComponent.vue -->
+
+<template>
+  <div class="greeting">{{ msg }}</div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('Hello World!')
+</script>
+
+<style scoped>
+.greeting {
+  color: red;
+}
+</style>
