b22dd2739b4863c5dbd089213a017f45592130ed
yoonsunho <y_sunho@naver.com>
2025-05-15 19:50:00 +0900

    pignia

---
+++ vue/07-state-management/vue-project/src/components/Example.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>State: {{ store.count }}</p>
+        <p>getters: {{ store.doubleCount }}</p>
+        <button @click="store.increment">Click</button>
+        <hr>
+
+    </div>
+</template>
+
+<script setup>
+   
+    import { useCounterStore } from '@/stores/counter';
+    const store = useCounterStore()
+    console.log(store.count)    //store.value.count x -> .value 생략
+</script>
+
+<style scoped>
+
+</style>
